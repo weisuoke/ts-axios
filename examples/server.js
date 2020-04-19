0fcea1ab9e86538068712291c62f944d85be8918
梅林的法杖 <530264127@qq.com>
2020-04-19 20:09:25 +0800

    feat(5.9):

--- examples/server.js
+++ examples/server.js
@@ -33,11 +33,11 @@ router.get('/simple/get', function(req, res) {
 })
 
 router.get('/base/get', function(req, res) {
-  return res.json(req.query)
+  res.json(req.query)
 })
 
 router.post('/base/post', function(req, res) {
-  return res.json(res.body)
+  res.json(req.body)
 })
 
 router.post('/base/buffer', function(req, res) {
