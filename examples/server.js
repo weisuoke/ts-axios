533bf004e820d5403ce6ba361a2d61cc467f33d5
梅林的法杖 <530264127@qq.com>
2020-04-19 18:07:27 +0800

    feat(5.5):

--- examples/server.js
+++ examples/server.js
@@ -22,7 +22,7 @@ app.use(webpackHotMiddleware(compiler))
 app.use(express.static(__dirname))
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const router = express.Router()
 
@@ -33,12 +33,29 @@ router.get('/simple/get', function(req, res) {
 })
 
 router.get('/base/get', function(req, res) {
-  res.json(req.query)
+  return res.json(req.query)
+})
+
+router.post('/base/post', function(req, res) {
+  return res.json(res.body)
+})
+
+router.post('/base/buffer', function(req, res) {
+  let msg = []
+  req.on('data', (chunk) => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
+  })
 })
 
 app.use(router)
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
 module.exports = app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}, Ctrl + C to stop`)
