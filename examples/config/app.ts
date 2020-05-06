1838d59a8034c8c0983cb6fc482887b704cb1ef8
梅林的法杖 <530264127@qq.com>
2020-05-06 22:39:57 +0800

    feat(9.7):

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -16,7 +16,26 @@ import qs from 'qs'
 //   console.log(res.data)
 // })
 
-axios({
+// axios({
+//   transformRequest: [(function(data) {
+//     return qs.stringify(data)
+//   }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
+//   transformResponse: [...(axios.defaults.transformResponse as AxiosTransformer[]), function(data) {
+//     if (typeof data === 'object') {
+//       data.b = 2
+//     }
+//     return data
+//   }],
+//   url: '/config/post',
+//   method: 'post',
+//   data: {
+//     a: 1
+//   }
+// }).then((res) => {
+//   console.log(res.data)
+// })
+
+const instance = axios.create({
   transformRequest: [(function(data) {
     return qs.stringify(data)
   }), ...(axios.defaults.transformRequest as AxiosTransformer[])],
@@ -25,7 +44,10 @@ axios({
       data.b = 2
     }
     return data
-  }],
+  }]
+})
+
+instance({
   url: '/config/post',
   method: 'post',
   data: {
